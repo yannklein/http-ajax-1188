6632ea451fb1bad64c386cfe82ce653052262e4a
Yann Klein <yann.klein@me.com>
2023-06-17 11:01:14 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@
 // Rehearsal
 // //////////////////////
 
-// // 1. Select elements (button)
+// 1. Select elements (button)
 // const button = document.querySelector("#click-me");
 
-// // 2. Listen to a click on the button
+// // 2. Listen to an event (click on the button)
 // button.addEventListener("click", (event) => {
 //   console.log(event);
-//   // 3. Change the DOM (disable, change the text)
-//   event.currentTarget.disabled = true;
-//   event.currentTarget.innerText = "Loading...";
+//   // 3. Change the DOM (add a disabled class, change the innerText)
+//   const clickedElement =  event.currentTarget;
+//   clickedElement.classList.add("disabled");
+//   clickedElement.innerText = "Loading...";
 // });
 
 
@@ -18,43 +19,42 @@
 // HTTP GET request
 // //////////////////////
 
-// 1. Select elements (input, button, list)
-console.log("Start of the JS");
-
-const input = document.querySelector("#keyword");
-const button = document.querySelector("#submit");
+// 1. Select elements (submit, keyword, results)
+console.log('start of the code');
+const submit = document.querySelector("#submit");
+const keyword = document.querySelector("#keyword");
 const results = document.querySelector("#results");
 
-// 2. Listen to a click on button
-button.addEventListener("click", (event) => {
-  // console.log(event);
-  console.log("button clicked");
+// 2. Listen to a click on submit
+submit.addEventListener("click", (event) => {
+  console.log('after the click');
 
-  event.preventDefault();
-  // 2.5 Fetch data from OMDBAPI
-  const keyword = input.value;
-  const url = `https://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
-  // const results = fetch(url) DOESNT WORK
+  event.preventDefault(); // prevent the default reload of the page
+  // console.log(event);
+  // 2.5 Ftech OMDB API, get the movies
+  const url = `https://www.omdbapi.com/?s=${keyword.value}&apikey=adf1f2d7`;
+  // const movies = fetch(url) //DOESNT WORK
   fetch(url)
-    .then( response => response.json())
-    .then( (data) => {
-      console.log("data arrives");
-      // console.log(data.Search);
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log('data arrives');
+      results.innerHTML = '';
+      // 3. Change the DOm, display the movies
       const movies = data.Search;
-      // 3. Change the DOM (show data on list)
-      results.innerHTML = "";
       movies.forEach((movie) => {
-        results.insertAdjacentHTML("beforeend", `
-          <li class='list-inline-item'>
+        results.insertAdjacentHTML(
+          "beforeend", 
+          `<li class='list-inline-item'>
             <img src="${movie.Poster}" alt="" />
             <p>${movie.Title}</p>
-          </li>
-        `)
+          </li>`
+        );
+      });
     });
-  })
-  console.log("after the fetch");
+    console.log('after the fetch');
 });
-
+console.log('end of the code');
 
 // //////////////////////
 // HTTP POST request
@@ -65,22 +65,25 @@ const signUp = (event) => {
   const emailValue = document.getElementById("email").value
   const passwordValue = document.getElementById("password").value
 
-  const data = {"email": emailValue, "password": passwordValue};
+  const data = {email: emailValue, password: passwordValue};
 
   const options = {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(data)
-  }
+  };
 
-  const url = "https://reqres.in/api/register";
-  
-  fetch(url, options)
+  // 2.5 Fecth dummy API (POST)
+  fetch("https://reqres.in/api/register", options)
     .then(response => response.json())
     .then((data) => {
+      // 3. Change the DOM
       console.log(data)
-    })
-}
+    });
 
-const form = document.querySelector("#form")
-form.addEventListener("submit", signUp)
+};
+
+// 1. Select elements
+const form = document.querySelector("#form");
+// 2. Listen to a submit event
+form.addEventListener("submit", signUp);
